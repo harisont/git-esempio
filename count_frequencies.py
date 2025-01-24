2cbfb59ab43fe44e8d3f5c76716c3f736e05d235
Arianna Masciolini <uzkamascio@gmail.com>
2025-01-24 11:51:17 +0100

    script conta frequenze

---
+++ count_frequencies.py
@@ -0,0 +1,13 @@
+freqdict = {}
+
+with open("alice.txt", encoding="utf-8") as fin:
+	for line in fin:
+		line = line.strip().split(" ")
+		for token in line:
+			if not token in freqdict:
+				freqdict[token] = 0
+
+			freqdict[token] = freqdict[token] + 1
+
+for token, frequency in freqdict.items():
+	print(f"{token}\t{frequency}")
